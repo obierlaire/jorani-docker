78df1c091637b9e9e6ba61b18d29c3f718b773a5
Olivier <obierlaire@gmail.com>
2017-02-24 16:12:26 +0100

    .

---
+++ email.php
@@ -0,0 +1,22 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed.');
+$config['protocol'] = 'smtp';
+$config['useragent'] = 'phpmailer';
+//$config['mailpath'] = '/usr/sbin/sendmail';
+$config['smtp_host'] = getenv('SMTP_HOST');
+$config['smtp_user'] = getenv('SMTP_USER');
+$config['smtp_pass'] = getenv('SMTP_PASSWORD');
+$config['_smtp_auth'] = TRUE;
+$config['smtp_port'] = getenv('SMTP_PORT');
+$config['smtp_timeout'] = '20';
+$config['smtp_crypto'] = getenv('SMTP_CRYPTO');                       // '' or 'tls' or 'ssl'
+$config['charset'] = 'UTF-8';
+$config['validate'] = true;
+$config['mailtype'] = 'html';
+$config['wordwrap'] = FALSE;
+$config['wrapchars'] = 70;
+$config['validate'] = FALSE;
+$config['priority'] = 3;                                // 1, 2, 3, 4, 5
+$config['newline'] = "\r\n";
+$config['crlf'] = "\r\n";
+$config['bcc_batch_mode'] = false;
+$config['bcc_batch_size'] = 200;
